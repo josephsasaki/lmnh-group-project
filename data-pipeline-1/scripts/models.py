4439c0c04f22dc3e585e9ee35a2cc4694c48a80a
AntuFTW <antarikshpatel2002@gmail.com>
2025-03-31 18:14:30 +0100

    Created Record class in models.py

--- data-pipeline-1/scripts/models.py
+++ data-pipeline-1/scripts/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Record:
     """Record class which helps store incoming data from API"""
 
@@ -6,16 +9,47 @@ class Record:
         self.__plant_id = record_dict_data.get('plant_id')
         self.__soil_moisture = Record.clean_soil_moisture(
             record_dict_data.get('soil_moisture'))
-        self.__temperature = record_dict_data.get('temperature')
-        self.__time = record_dict_data.get('recording_taken')
+        self.__temperature = Record.clean_temperature(
+            record_dict_data.get('temperature'))
+        self.__time = Record.clean_time(
+            record_dict_data.get('recording_taken'))
 
     @staticmethod
-    def clean_soil_moisture(soil_moisture_data_in: float):
-        if soil_moisture_data_in is None:
-            raise ValueError(f'Soil moisture not included in record data')
+    def clean_plant_id(plant_id_in: int) -> int:
+        if plant_id_in is None:
+            raise KeyError(f'Input data does not have a plant_id')
 
-        if soil_moisture_data_in > 100 or soil_moisture_data_in < 0:
+        return plant_id_in
+
+    @staticmethod
+    def clean_soil_moisture(soil_moisture_in: float) -> float:
+        if soil_moisture_in is None:
+            raise KeyError(f'Soil moisture not included in record data')
+
+        if soil_moisture_in > 100 or soil_moisture_in < 0:
             raise ValueError(
-                f'Soil moisture level of {soil_moisture_data_in} is not valid')
+                f'Soil moisture level of {soil_moisture_in} is not valid')
 
-        return soil_moisture_data_in
+        return soil_moisture_in
+
+    @staticmethod
+    def clean_temperature(temperature_in: float) -> float:
+        if temperature_in is None:
+            raise KeyError(f'Temperature not included in record data')
+
+        return temperature_in
+
+    @staticmethod
+    def clean_time(time_in: str) -> datetime:
+        if time_in is None:
+            raise KeyError(f'Time not included in record data')
+
+        try:
+            time_in = datetime.strptime(time_in, "%Y-%m-%d %H:%M:%S")
+            if time_in > datetime.now():
+                raise ValueError(
+                    f'The time ({time_in}) is invalid as it is in the future')
+            return time_in
+        except ValueError:
+            raise ValueError(
+                f'The time ({time_in} is in an invalid format. Format should be "%Y-%m-%d %H:%M:%S")')
