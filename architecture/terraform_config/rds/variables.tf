79d912c6a5a831050f40b2019cad5bfc9ba796dd
Josh Allen <trainee.josh.allen@sigmalabs.co.uk>
2025-04-01 13:22:56 +0100

    #40 erd names corrected and #3 rds made dynamic

--- architecture/terraform_config/rds/variables.tf
+++ architecture/terraform_config/rds/variables.tf
@@ -6,4 +6,22 @@ variable "DB_PASSWORD" {
   type = string
 }
 
+variable "REGION" {
+  type = string
+}
 
+variable "VPC_ID" {
+  type = string
+}
+
+variable "SG_NAME" {
+  type = string
+}
+
+variable "RDS_NAME" {
+  type = string
+}
+
+variable "RDS_SUBNET_GROUP_NAME" {
+  type = string
+}
