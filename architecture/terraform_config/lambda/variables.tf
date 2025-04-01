8c4c9683f5be0c7b0442eb13eeef15f5b4adeb26
Josh Allen <trainee.josh.allen@sigmalabs.co.uk>
2025-04-01 20:50:07 +0100

    lambda variables added

---
+++ architecture/terraform_config/lambda/variables.tf
@@ -0,0 +1,40 @@
+variable "DB_USERNAME" {
+  type = string
+}
+
+variable "DB_PASSWORD" {
+  type = string
+}
+
+variable "REGION" {
+  type = string
+}
+
+
+variable "VPC_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_KEY" {
+  type = string
+}
+
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+
+variable "PIPELINE_NAME" {
+  type = string
+}
+
+variable "PIPELINE2_NAME" {
+  type = string
+}
+
+variable "LAMBDA_NAME1" {
+  type = string
+}
+
+variable "LAMBDA_NAME2" {
+  type = string
+}
