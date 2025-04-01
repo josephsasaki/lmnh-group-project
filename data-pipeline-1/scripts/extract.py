76337d4834f2bc7b26a5dba4a8b2a6b340c443f0
josephsasaki <joseph.sasaki.314@gmail.com>
2025-04-01 16:38:00 +0100

    Completed extract for data pipeline 1

--- data-pipeline-1/scripts/extract.py
+++ data-pipeline-1/scripts/extract.py
@@ -8,8 +8,9 @@ class Extract:
 
     API_ENDPOINT = "https://data-eng-plants-api.herokuapp.com/plants/"
     MIN_PLANT_ID = 1
-    MAX_PLANT_ID = 10
+    MAX_PLANT_ID = 55
 
+    @staticmethod
     def _make_request(plant_id: int) -> dict:
         '''Given a plant_id, make a get request to the endpoint and return the result.'''
         request = requests.get(Extract.API_ENDPOINT+str(plant_id))
@@ -23,19 +24,4 @@ class Extract:
         plant_ids = list(range(Extract.MIN_PLANT_ID, Extract.MAX_PLANT_ID+1))
         with mp.Pool(mp.cpu_count()) as p:
             fetched_data = p.map(Extract._make_request, plant_ids)
-        return [json_obj for json_obj in fetched_data if not None]
-
-    @staticmethod
-    def extract_slow():
-        '''Extract the endpoint data for all ids in the specified range.'''
-        plant_ids = list(range(Extract.MIN_PLANT_ID, Extract.MAX_PLANT_ID+1))
-        fetched_data = []
-        for plant_id in plant_ids:
-            fetched_data.append(Extract._make_request(plant_id))
-        return [json_obj for json_obj in fetched_data if not None]
-
-
-if __name__ == "__main__":
-    end = time.time()
-    x = Extract.extract_slow()
-    print(x)
+        return [json_obj for json_obj in fetched_data if json_obj is not None]
