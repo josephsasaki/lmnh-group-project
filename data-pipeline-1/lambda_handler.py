284478fdc2f8c42b3db818db28d8c9100aa66325
josephsasaki <joseph.sasaki.314@gmail.com>
2025-04-03 17:02:39 +0100

    Completed dockerising

---
+++ data-pipeline-1/lambda_handler.py
@@ -0,0 +1,18 @@
+import logging
+from pipeline import run_api_pipeline
+
+
+def lambda_handler(event: None, context: None):
+    '''This function is called by the AWS Lambda to run the Python scripts.'''
+    logger = logging.getLogger()
+    logger.setLevel("INFO")
+    try:
+        logger.info("Running...")
+        run_api_pipeline()
+        logger.info("Successful")
+        return {
+            'status_code': 200
+        }
+    except Exception as e:
+        logger.error("Error: %s", e)
+        raise
